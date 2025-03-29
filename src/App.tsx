07bea194c72d04966b30d1364ff3277b9557fc8c
Simran <itssimran903@gmail.com>
2025-03-29 17:55:05 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import Login from './pages/Login';
+import UserList from './pages/UserList';
+import EditUser from './pages/EditUser';
 
+const App: React.FC = () => {
   return (
-    <div className="text-red-500 text-2xl">
-      Hello
-    </div>
-  )
-}
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          
+          <Route element={<PrivateRoute />}>
+            <Route path="/users" element={<UserList />} />
+            <Route path="/users/:id/edit" element={<EditUser />} />
+          </Route>
+          
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
